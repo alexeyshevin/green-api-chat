6cc2aabe675dc2ab01575f1ce403a5a3b28d71c3
alexeyshevin <ashevin92@gmail.com>
2026-09-24 16:45:32 +0700

    message list

---
+++ src/components/Chat/MessageList/MessageList.tsx
@@ -0,0 +1,29 @@
+import type { Message } from '../../../types/message';
+
+type Props = {
+  messages: Message[];
+};
+
+export const MessageList = ({ messages }: Props) => {
+  return (
+    <div className="message-list">
+      {messages.map((message) => (
+        <div
+          key={message.id}
+          className={`message message--${message.direction}`}
+        >
+          <p>{message.text}</p>
+
+          <time>
+            {new Date(
+              message.timestamp,
+            ).toLocaleTimeString([], {
+              hour: '2-digit',
+              minute: '2-digit',
+            })}
+          </time>
+        </div>
+      ))}
+    </div>
+  );
+};
